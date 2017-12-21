7610e27ac40f84cb0a74d90545f4e47564827367
zhangheng19931123 <zhangheng19931123@gmail.com>
2017-12-21 14:07:57 +0100

    play the video after conversion

--- video/img2video.py
+++ video/img2video.py
@@ -1,9 +1,9 @@
 import cv2
 import os, sys, getopt
 import glob
+import time
 
-def main():
-    video_name = sys.argv[1]
+def generate_video(video_name):
     image_folder = 'output'
     images = [img for img in os.listdir(image_folder) if img.endswith(".jpg")]
     count = 0
@@ -19,6 +19,23 @@ def main():
         video.write(cv2.imread(os.path.join(image_folder, image)))
     cv2.destroyAllWindows()
     video.release()
+
+def play_video(video_name):
+    cap = cv2.VideoCapture(video_name)
+    while(True):
+        ret, frame = cap.read()
+        cv2.imshow('frame',frame)
+        time.sleep(0.05)    #20fps
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+    cap.release()
+    cv2.destroyAllWindows()
+
+def main():
+    video_name = sys.argv[1]
+    generate_video(video_name)
+    play_video(video_name)
+
 
 if __name__ == "__main__":
    main()
